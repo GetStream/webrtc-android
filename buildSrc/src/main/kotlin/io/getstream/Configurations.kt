e213ed1ed66609b08a330b720ee66b874037862c
skydoves <skydoves@naver.com>
2023-09-19 13:40:10 +0900

    Bump AGP, Compose, landscapist and androidx dependencies

--- buildSrc/src/main/kotlin/io/getstream/Configurations.kt
+++ buildSrc/src/main/kotlin/io/getstream/Configurations.kt
@@ -1,8 +1,8 @@
 package io.getstream
 
 object Configurations {
-    const val compileSdk = 33
-    const val targetSdk = 33
+    const val compileSdk = 34
+    const val targetSdk = 34
     const val minSdk = 21
     const val majorVersion = 1
     const val minorVersion = 0
