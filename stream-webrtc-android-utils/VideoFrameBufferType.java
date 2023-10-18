81fd308a858babfe89ca666cf762f0b7be240b58
skydoves <skydoves@naver.com>
2023-10-18 09:16:39 +0900

    Add util files into the stream-webrtc-android-utils path

---
+++ stream-webrtc-android-utils/VideoFrameBufferType.java
@@ -0,0 +1,16 @@
+package org.webrtc;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface VideoFrameBufferType {
+  int NATIVE = 0;
+  int I420 = 1;
+  int I420A = 2;
+  int I422 = 3;
+  int I444 = 4;
+  int I010 = 5;
+  int I210 = 6;
+  int NV12 = 7;
+}
